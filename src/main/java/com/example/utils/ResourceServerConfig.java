3a0fc628517115f135cf9f213fdf0694e0dc91a3
VernonJS <165072139+VernonJS@users.noreply.github.com>
2024-03-27 10:56:53 +0530

    Update ResourceServerConfig.java

--- src/main/java/com/example/utils/ResourceServerConfig.java
+++ src/main/java/com/example/utils/ResourceServerConfig.java
@@ -16,50 +16,18 @@ import com.fasterxml.jackson.databind.JsonNode;
 public class ResourceServerConfig {
 
     //YOUR IDENTITY DOMAIN AND APPLICATION CREDENTIALS
-    private String IDCS_URL;
+    private String AS2_SERVICEUSER_B64;
 
-
-    //OIC CLIENT CREDENTIALS
-    private String OIC_CLIENT_ID;
-    private String OIC_CLIENT_SECRET;
-    private String OIC_CLIENT_SCOPE;
-
-    //INFORMATION ABOUT IDENTITY CLOUD SERVICES
-    private  String JWK_URL ;
-    private  String TOKEN_URL ;
-
-    public String getIDCS_URL() {
-        return IDCS_URL;
+    public String getAS2_SERVICEUSER_B64() {
+        return AS2_SERVICEUSER_B64;
     }
 
-    public String getOIC_CLIENT_ID() {
-        return OIC_CLIENT_ID;
-    }
 
-    public String getOIC_CLIENT_SECRET() {
-        return OIC_CLIENT_SECRET;
-    }
-
-    public String getOIC_CLIENT_SCOPE() {
-        return OIC_CLIENT_SCOPE;
-    }
-
-    public String getJWK_URL() {
-        return JWK_URL;
-    }
-
-    public String getTOKEN_URL() {
-        return TOKEN_URL;
-    }
 
     public  ResourceServerConfig(JsonNode secretContents) {
 
-        IDCS_URL = secretContents.get("idcs_base_url").textValue();
-        OIC_CLIENT_ID = secretContents.get("client_id").textValue();
-        OIC_CLIENT_SECRET = secretContents.get("client_secret").textValue();
-        OIC_CLIENT_SCOPE = secretContents.get("scope").textValue();
 
-       JWK_URL = IDCS_URL + "/admin/v1/SigningCert/jwk";
-       TOKEN_URL = IDCS_URL + "/oauth2/v1/token";
+        AS2_SERVICEUSER_B64 = secretContents.get("AS2_ServiceUser").textValue();
+
     }
 }
